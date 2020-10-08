ed68b01b47328c2e470edf5f26744b97321253ac
Gauthier PEEL <gauthier.peel@gmail.com>
2020-10-08 18:57:15 +0200

    migrating nextPlayer() method

--- src/main/java/trivia/thegame/Board.java
+++ src/main/java/trivia/thegame/Board.java
@@ -3,11 +3,17 @@ package trivia.thegame;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
+/**
+ * The Board class responsability is the layout and position of the board
+ * ie what a cell category is, and where the Player(s) are.
+ */
 @Component
 @Scope(SCOPE_PROTOTYPE)
 public class Board {
@@ -17,15 +23,33 @@ public class Board {
                     "Sports", "Rock", "Pop", "Science", "Sports", "Rock"};
 
     private Map<Player, Integer> cellNumberForPlayerMap = new HashMap<>();
+    private List<Player> players = new ArrayList<>();
+    private Player currentPlayer;
 
     public Board() {
         System.err.println("NEW instance of BOARD !!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 
-    public void addPlayer(Player player) {
-        cellNumberForPlayerMap.put(player, new Integer(0));
+    public void addPlayerForName(String playerName) {
+        Player player = new Player(playerName);
+        players.add(player);
+        cellNumberForPlayerMap.put(player, 0);
         System.out.println(player.getName() + " was added");
         System.out.println("They are player number " + cellNumberForPlayerMap.size());
+    }
+
+    public int getNumberOfPlayers() {
+        return players.size();
+    }
+
+    public Player nextCurrentPlayer() {
+        if (currentPlayer == null) {
+            currentPlayer = players.get(0);
+        } else {
+            currentPlayer = players.get((players.indexOf(currentPlayer) + 1) % players.size());
+        }
+        System.out.println(currentPlayer.getName() + " is the current player");
+        return currentPlayer;
     }
 
     public String getCellCategory(int cellNumber) {
@@ -42,7 +66,7 @@ public class Board {
 
     public void movePlayerWithRoll(Player player, int roll) {
         int newCellForPlayer = (cellNumberForPlayerMap.get(player) + roll) % 12;
-        cellNumberForPlayerMap.put(player, new Integer(newCellForPlayer));
+        cellNumberForPlayerMap.put(player, newCellForPlayer);
         System.out.println(player.getName() + "'s new location is " + cellNumberForPlayerMap.get(player));
         System.out.println("The category is " + getCategoryForPlayer(player));
     }
--- src/main/java/trivia/thegame/Game.java
+++ src/main/java/trivia/thegame/Game.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
+/**
+ * The Game class responsability is defining the RULES of the Game
+ */
 @Component
 @Scope(SCOPE_PROTOTYPE)
 public class Game {
@@ -20,8 +21,6 @@ public class Game {
 
 
     private final Board board;
-
-    List<Player> players = new ArrayList();
     Player currentPlayer;
 
     LinkedList popQuestions = new LinkedList();
@@ -50,25 +49,18 @@ public class Game {
         return "Rock Question " + index;
     }
 
-    public boolean isPlayable() {
-        return (players.size() >= 2);
-    }
 
     public void add(String playerName) {
-        if (players.size() >= MAX_PLAYERS) {
-            throw new RuntimeException("The limit MAX of Player is already reached!");
+        // this is a RULE, so it's here in the Game class
+        if (board.getNumberOfPlayers() >= MAX_PLAYERS) {
+            throw new RuntimeException("The limit MAX of Player of "
+                    + MAX_PLAYERS + " is already reached!");
         }
-        Player player = new Player(playerName);
-        players.add(player);
-        board.addPlayer(player);
-        if (players.size() == 1) {
-            currentPlayer = players.get(0);
-        }
-
+        board.addPlayerForName(playerName);
     }
 
     public void roll(int roll) {
-        System.out.println(currentPlayer.getName() + " is the current player");
+        currentPlayer = board.nextCurrentPlayer();
         System.out.println("They have rolled a " + roll);
 
         if (currentPlayer.isInPenaltyBox()) {
@@ -76,33 +68,35 @@ public class Game {
                 System.out.println(currentPlayer.getName() + " is not getting out of the penalty box");
                 hasTheRightToGetOutOfThePenaltyBoxAndAskedAQuestion = false;
             } else {
+                System.out.println(currentPlayer.getName() + " is getting out of the penalty box");
                 hasTheRightToGetOutOfThePenaltyBoxAndAskedAQuestion = true;
 
-                System.out.println(currentPlayer.getName() + " is getting out of the penalty box");
-
                 board.movePlayerWithRoll(currentPlayer, roll);
-
                 askQuestion();
             }
-
         } else {
             board.movePlayerWithRoll(currentPlayer, roll);
             askQuestion();
         }
-
     }
 
 
     private void askQuestion() {
-        String cat = board.getCategoryForPlayer(currentPlayer);
-        if (cat.equals("Pop"))
-            System.out.println(popQuestions.removeFirst());
-        if (cat.equals("Science"))
-            System.out.println(scienceQuestions.removeFirst());
-        if (cat.equals("Sports"))
-            System.out.println(sportsQuestions.removeFirst());
-        if (cat.equals("Rock"))
-            System.out.println(rockQuestions.removeFirst());
+        String categoryOfAskedQuestion = board.getCategoryForPlayer(currentPlayer);
+        switch (categoryOfAskedQuestion) {
+            case "Pop":
+                System.out.println(popQuestions.removeFirst());
+                break;
+            case "Science":
+                System.out.println(scienceQuestions.removeFirst());
+                break;
+            case "Sports":
+                System.out.println(sportsQuestions.removeFirst());
+                break;
+            case "Rock":
+                System.out.println(rockQuestions.removeFirst());
+                break;
+        }
     }
 
 
@@ -120,20 +114,13 @@ public class Game {
             currentPlayer.printPlayerBankAccount();
         }
         boolean winner = !currentPlayer.isWinner(NUMBER_OF_COINS_TO_WIN);
-        nextPlayer();
         return winner;
     }
 
     public boolean wrongAnswer() {
         System.out.println("Question was incorrectly answered");
         currentPlayer.goToPenalTyBox();
-        nextPlayer();
         return true;
     }
-
-    public void nextPlayer() {
-        currentPlayer = players.get((players.indexOf(currentPlayer) + 1) % players.size());
-    }
-
 
 }
